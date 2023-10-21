ca0818af9b34379665d5692d74f2502df55455e3
Angela Maria <amcucunubar@gmail.com>
2023-10-21 16:52:04 +0200

    Dash, edicion de grafico y radioitems

--- app.py
+++ app.py
@@ -1,58 +1,71 @@
-from dash import Dash, html, dash_table, dcc
+from dash import Dash, html, dash_table, dcc, callback, Output, Input
 import pandas as pd
 import plotly.express as px
 
 # Incorporate data
 df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/gapminder2007.csv')
 
-# Initialize the app
+# iniciar-crear la app
 app = Dash(__name__)
 
 colors = {
-    'background': '#ece9e5',
-    'text': '#ff7f7f'
+    'background': '#c1bebe',
+    'text': '#ff7f7f',
+    'fond': '#880E4F'
 }
 # dataframe 
 df = pd.DataFrame({
     "Fruit": ["Pommes", "Oranges", "Bananas", "Pommes", "Oranges", "Bananas", "Bananas"],
-    "Amount": [4, 1, 2, 2, 4, 5, 3],
-    "City": ["Bordeaux", "Bordeaux", "Bordeaux", "Lile", "Lile", "Lile", "Nantes"]
+    "Quantité": [4, 1, 2, 2, 4, 5, 3],
+    "Ville": ["Bordeaux", "Bordeaux", "Bordeaux", "Lile", "Lile", "Lile", "Nantes"]
 })
 
-fig = px.bar(df, x="City", y="Amount", color="Fruit", barmode="group", title="Des Fruits")
+#fig = px.bar(df, x="Ville", y="Quantité", color="Fruit", barmode="group", title="Des Fruits")
 
-# App layout
-app.layout = html.Div(style={'backgroundColor': colors['background']}, children=[
+# Crear el App layout, es el diseño de como se va a ver la aplicación
+#Baner o titulo = H1, radioitems, graficos
+app.layout = html.Div(className='row', style={'backgroundColor': colors['background']}, children=[
     html.H1(
         children='Hello Dash',
-        style={
-            'textAlign': 'center',
-            'color': colors['text']
+        style={'textAlign': 'center',
+            'color': colors['text'],
         }
     ),
 
-    html.Div(children='Dash: mon premier essaie.', style={
-        'textAlign': 'center',
-        'color': colors['text']
-    }),
+    html.H2(children='Dash: mon premier essaie.', 
+             style={'textAlign': 'center',
+                    'color': colors['fond'],
+    }), 
 
-    dcc.Graph(
-        id='example-graph-2',
-        figure=fig
-    )
- ])
-# app.layout = html.Div(children=[
-#     html.H1(children='Hello Dash'),
+    dash_table.DataTable(data=df.to_dict('records'), page_size=10, style_table={'overflowX': 'auto'}),
+#Separa la informacion 
+    html.Hr(),
 
-#     html.Div(children='''
-#         Dash: mon premier essaie.
-#     '''),
+    html.Div([
+        html.Div([
+            html.H3('Choisir la variable', className= 'fix-label', style={'textAlign': 'center','color': 'red', 'margin-top':'35px'} ),
+            dcc.RadioItems(id='controls-and-radio-item', 
+                           labelStyle = {'display': 'inline-block'},
+                           options=['Ville', 'Quantité', 'Fruit'], 
+                           value='Fruit', 
+                           style={'color': colors['text'], 'margin-top':'50px'}, className = 'dcc_compon'),  
+                            ], className= 'create_container2 five columns', style = {'margin-bottom': '50px'})
+                            ], className = 'row flex-display',),
+                               
+    html.Div([
+        dcc.Graph(figure={}, id='controls-and-graph')]),
 
-#     dcc.Graph(
-#         id='example-graph',
-#         figure=fig
-#     )
-# ])
-# Run the app
+])
+
+# Añadir controles para crear la interacción
+@callback(
+    Output(component_id='controls-and-graph', component_property='figure'),
+    Input(component_id='controls-and-radio-item', component_property='value')
+)
+def update_graph(col_chosen):
+    fig = px.bar(df, x="Ville", y=col_chosen, barmode="group", title="Des Fruits")
+    return fig
+
+# Ejecutar la app
 if __name__ == '__main__':
     app.run(debug=True)
