66a63847814f2dda5a30e6b7383856e1abd061e9
Angela Maria <amcucunubar@gmail.com>
2023-10-27 17:36:24 +0200

    archivos

--- app2.py
+++ app2.py
@@ -1,43 +1,99 @@
-from dash import Dash, html, dash_table, dcc, callback, Output, Input
+from dash import Dash, html, dash_table, callback, Output, Input
+from dash import dcc
 import pandas as pd
 import plotly.express as px
 
 #importar data
-df = pd.read_csv('https://gist.githubusercontent.com/chriddyp/c78bf172206ce24f77d6363a2d754b59/raw/c353e8ef842413cae56ae3920b8fd78468aa4cb2/usa-agricultural-exports-2011.csv')
+df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/gapminderDataFiveYear.csv')
 
 # iniciar-crear la app
 app = Dash(__name__)
 
 app.layout = html.Div([
+    html.Div([
+        html.Img(src='https://us.123rf.com/450wm/grgroup/grgroup1611/grgroup161100278/64919022-ic%C3%B4ne-de-la-tour-de-l-usine-de-p%C3%A9trole-sur-fond-blanc-illustration-vectorielle.jpg?ver=6',  
+            width="90", height="70", 
+            style = {'float':'right', 
+                     'background-repeat': 'repeat',
+                      'background-size': 'contain',
+                            },),
+        html.H1("CURVES PUMPS COLOMBIA", 
+                style= {'textAlign': 'center',
+                    'color': '#ffffff', 
+                    'background-color': '#07083b',
+                    'padding': '10px',
+                   }),
+    ]),
+    
+    html.Div(children=[
+        html.Label(html.H2('Choisir le well'), style= {'margin-bottom': '0.67em'}),
+        html.Hr(), 
+        dcc.Dropdown(options=[{'label': well, 'value': well} for well in df['continent'].unique()], 
+                     id= 'pandas-dropdown-1',),
+        html.Div(id= 'pandas-output-container-1'),
+        ]),
+    html.Hr(),
+    html.Div(
+     dash_table.DataTable(
+            id='data-table', page_size=10,
+            columns=[{'name': col, 'id': col} for col in df.columns]
+            ),
+        ),
 
     html.Div([
-        html.H1("CURVES PUMPS PERENCO COLOMBIA", 
-                style= {'textAlign': 'center',
-                    'color': '#686363'}),
-        html.Img(src='Perenco.jpg')]),
+        html.Div([
+            html.Div([
+                dcc.Dropdown(
+                    df['Indicator Name'].unique(),
+                    'Fertility rate, total (births per woman)',
+                    id='xaxis-column'
+                ),
+                dcc.RadioItems(
+                    ['Linear', 'Log'],
+                    'Linear',
+                    id='xaxis-type',
+                    inline=True
+                )
+            ], style={'width': '48%', 'display': 'inline-block'}),
+
+            html.Div([
+                dcc.Dropdown(
+                    df['Indicator Name'].unique(),
+                    'Life expectancy at birth, total (years)',
+                    id='yaxis-column'
+                ),
+                dcc.RadioItems(
+                    ['Linear', 'Log'],
+                    'Linear',
+                    id='yaxis-type',
+                    inline=True
+                )
+            ], style={'width': '48%', 'float': 'right', 'display': 'inline-block'})
+        ]),
+
+        dcc.Graph(id='indicator-graphic'),
+
+        dcc.Slider(
+            df['Year'].min(),
+            df['Year'].max(),
+            step=None,
+            id='year--slider',
+            value=df['Year'].max(),
+            marks={str(year): str(year) for year in df['Year'].unique()},
+
+        )
+    ])
 
 ])
+@app.callback(
+    Output(component_id='data-table', component_property= 'data'),
+    [Input(component_id='pandas-dropdown-1', component_property= 'value')]
+)
+def update_table(selected_well):
+    filtered_df = df[df['continent'] == selected_well]
+    return filtered_df.to_dict('records')
 
 # puedo cambiar el html de salida asignando un numero de 4 cifras, sin comas ni puntos
 if __name__ == '__main__':
     app.run(debug=True)
 
-#    html.Div([
-#         dash_table.DataTable(
-#         data=df.to_dict('records'),
-#         columns=[{'id': c, 'name': c} for c in df.columns],
-#         page_size=10, 
-#         style_cell={'minWidth': 95, 'maxWidth': 95, 'width': 95
-#     }),
-#     ]),
-    
-#     dcc.Dropdown(df.columns, id = 'pandas-dropdown-1'),
-#     html.Div(id= 'pandas-output-container-1')
-# ])
-# @callback(
-#         Output('pandas-output-container-1', 'children'),
-#         Input('pandas-dropdown-1', 'value')
-# )
-
-# def update_output(value):
-#     return f'You have selected {value}'
--- main_final.py
+++ main_final.py
@@ -3,14 +3,14 @@ from datetime import datetime
 from openpyxl import load_workbook
 
 # Ruta y el nombre del archivo .xlsm
-archivo_xlsm = ('PREDIC_SET.xlsm')
+# archivo_xlsm = ('PREDIC_SET.xlsm')
 
-# Cargar el archivo .xlsm
-workbook = load_workbook(archivo_xlsm, keep_vba=False)  # Establece keep_vba en False para eliminar las macros
+# # Cargar el archivo .xlsm
+# workbook = load_workbook(archivo_xlsm, keep_vba=False)  # Establece keep_vba en False para eliminar las macros
 
-# Guardar el archivo como .xlsx
-archivo_xlsx = 'archivo.xlsx'
-workbook.save(archivo_xlsx)
+# # Guardar el archivo como .xlsx
+# archivo_xlsx = 'archivo.xlsx'
+# workbook.save(archivo_xlsx)
 
 archivo_excel = 'C:\\Users\\User\\Desktop\\Programming\\Curves_pumps\\archivo.xlsx'
 documento_base = pd.read_excel(archivo_excel, sheet_name= None)
@@ -22,6 +22,14 @@ for key, hoja in documento_base.items():
     hoja.drop([0], inplace= True) #elimina la primera fila de datos
     hoja.insert(0, 'WELL', value= key)#insertar la columna con el nombre de cada pozo
     hoja.ffill(inplace=True) #rellenar los datos vacios con el numero anterior.
+
+a_eliminar = ['Unnamed: 50', 'Unnamed: 51', 'Unnamed: 52',
+            'FECHA.1', 'FRECUENCIA.1', 'BWPD', 'DIFERENCIA BOPD',
+            'DIFERENCIA BWPD', 'BOPD', 'BFPD.1','Q. OIL',
+            'DIFERENCIA BFPD', '%BSW', '°API', 'NETOS', 'Q. WATER']
+for key, hoja in documento_base.items():
+    columnas_a_eliminar = [col for col in a_eliminar if col in hoja.columns]
+    hoja.drop(columns=columnas_a_eliminar, inplace=True)
 
 info_list = []
 for info in documento_base.values():
@@ -36,9 +44,9 @@ dataframe1 = pd.concat(info_list, axis=0, ignore_index=True )
 valor_a_eliminar = 'DESPUES DE INGRESAR LOS PRIMEROS DATOS BORRAR LAS CELDAS EN AMARILLO CON DELETE CELLS Y UP'
 dataframe1 = dataframe1[dataframe1['FRECUENCIA'] != valor_a_eliminar]
 
-valores_a_reemplazar = ['nd', 'NO', '1211-9', 'fds', '-', 'FDS', 'cñg8d', 'ND', ' - ', '0.86|', 
+valores_a_reemplazar = ['nd', 'BFPD 4405' ,'NO', '1211-9', 'fds', '-', 'FDS', 'cñg8d', 'ND', ' - ', '0.86|', 
                         'SIN DATOS', 'sin datos', 'o.45', "299'", 'No lectura', 'No medido', 
-                        'No registra', ' ']
+                        'No registra', ' ', '                                       ', '0.6 POR 0.3']
 
 for columna in dataframe1.columns:
     if columna == 'WELL' or columna == 'FECHA':
@@ -56,4 +64,5 @@ for columna in dataframe1.columns:
     if dataframe1[columna].dtype == 'object':
         dataframe1[columna] = dataframe1[columna].astype(float)
 
-print(dataframe1) 
+dataframe1.to_csv('curves_pumps_final.csv')
+print(type(dataframe1))
